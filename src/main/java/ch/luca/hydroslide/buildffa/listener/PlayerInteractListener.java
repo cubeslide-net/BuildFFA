dfc9e5a91a03e06722a69e98d2f77c7c27888e7a
Timeox2k <schockilp@gmail.com>
2022-08-17 22:23:49 +0200

    Fishing rod fix

--- src/main/java/ch/luca/hydroslide/buildffa/listener/PlayerInteractListener.java
+++ src/main/java/ch/luca/hydroslide/buildffa/listener/PlayerInteractListener.java
@@ -1,6 +1,9 @@
 package ch.luca.hydroslide.buildffa.listener;
 
 import ch.luca.hydroslide.buildffa.BuildFFA;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,7 +18,18 @@ public class PlayerInteractListener implements Listener {
 			if(e.getClickedBlock().getType().isInteractable()) {
 				e.setCancelled(true);
 			}
-
 		}
+
+
+		if(e.getAction() == Action.RIGHT_CLICK_AIR ||e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			final Player player = e.getPlayer();
+
+			if(player.getInventory().getItemInMainHand().getType() == Material.FISHING_ROD) {
+				if(player.getLocation().getBlockY() >= BuildFFA.getInstance().getFightHeight()) {
+					e.setCancelled(true);
+				}
+			}
+		}
+
 	}
 }
