4df7bca4e3d63735879cc85e4278935db876f357
Luca <lucadubi@gmail.com>
2022-08-22 08:09:10 +0200

    fix english text

--- src/main/java/ch/luca/hydroslide/buildffa/listener/PlayerJoinListener.java
+++ src/main/java/ch/luca/hydroslide/buildffa/listener/PlayerJoinListener.java
@@ -18,7 +18,7 @@ public class PlayerJoinListener implements Listener {
 		Player p = e.getPlayer();
 		e.setJoinMessage(null);
 		p.sendMessage(BuildFFA.getInstance().getPrefix() + "Use §e/inv §7to sort your inventory.");
-		p.sendMessage(BuildFFA.getInstance().getPrefix() + "§cTeaming with other players are not allowed!");
+		p.sendMessage(BuildFFA.getInstance().getPrefix() + "§cTeaming with other players is not allowed!");
 		User user = BuildFFA.getInstance().getUserManager().getUser(p);
 		Location loc = BuildFFA.getInstance().getSpawnLocation();
 		p.teleport(loc != null ? loc : Bukkit.getWorlds().get(0).getSpawnLocation());
